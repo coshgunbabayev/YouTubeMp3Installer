6a07b058d3a1e4a0887277d4265d8f25ce88550e
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-06-21 07:04:45 +0400

    v2

--- bot.js
+++ bot.js
@@ -51,3 +51,8 @@ bot.launch((err) => {
     }
     console.log("Bot starting...");
 });
+
+const app = require("express")();
+app.listen(process.env.PORT, () => {
+    console.log("server is listening on port", process.env.PORT);
+});
